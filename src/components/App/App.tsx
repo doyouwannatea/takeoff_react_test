22aa3851884f1498298b86843313f43ece0177fe
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2022-05-05 20:52:47 +0800

    добавил необходимые зависимости * react-router@6 * redux-toolkit * @mui/material * json-server

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@mui/material';
 import styles from './App.module.css';
 
 const App = () => {
-  return <div className={styles.app}>React app</div>;
+  return (
+    <div className={styles.app}>
+      <Button>кнопка</Button>
+    </div>
+  );
 };
 
 export default App;
