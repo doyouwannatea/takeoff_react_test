4d0b5420464e3e4e06057b1e3373d34882adf1ff
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2022-05-05 19:06:23 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Тестовое задание Takeoff (react)
+
+## Запуск
+
+`node: v16.13.2`
+
+1. npm i
+2. npm run dev
---
+++ src/components/App/App.tsx
@@ -0,0 +1,7 @@
+import styles from './App.module.css';
+
+const App = () => {
+  return <div className={styles.app}>React app</div>;
+};
+
+export default App;
